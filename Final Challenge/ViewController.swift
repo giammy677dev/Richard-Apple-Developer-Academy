09f005ec5ace1464e9b3530d3f3d2d281b8a35fc
Gian Marco Orlando <gian.orlando@studenti.unina.it>
2018-05-14 10:00:16 +0200

    First commit

---
+++ Final Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Challenge
+//
+//  Created by Gian Marco Orlando on 14/05/18.
+//  Copyright © 2018 Gian Marco Orlando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Final ChallengeTests/Final_ChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Final_ChallengeTests.swift
+//  Final ChallengeTests
+//
+//  Created by Gian Marco Orlando on 14/05/18.
+//  Copyright © 2018 Gian Marco Orlando. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Challenge
+
+class Final_ChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
