f752576acd03439f49a3b799e4dfa85a3cbae68b
Stefano Formicola <stefano_17sf@libero.it>
2018-05-22 12:29:25 +0200

    Tag methods add(:forTag:) and remove(:forTag:)

--- Final Challenge/Model/Tag.swift
+++ Final Challenge/Model/Tag.swift
@@ -16,12 +16,17 @@ class Tag {
     private init() {}
     
     func add<T: Taggable>(_ obj: T, forTag tag: String) {
-        
+        /// Adds a reference of a taggable element for a tag
+        guard var arrayTag = items[tag] as? [T] else { items[tag] = [obj]; return }
+        if (arrayTag.contains(obj)) {
+            arrayTag.append(obj)
+        }
     }
     
     func remove<T: Taggable>(_ obj: T, forTag tag: String) {
+        /// Removes a reference of a taggable element for a tag
         guard var itemArray = Tag.shared.items[tag] as? [T] else { return }
-        if let index = itemArray.index(where: { $0 == obj }) {
+        if let index = itemArray.index(of: obj) {
             itemArray.remove(at: index)
         }
     }
