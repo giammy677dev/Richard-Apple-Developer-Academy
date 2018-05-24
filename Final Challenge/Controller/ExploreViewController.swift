e672ba80e0ae8cc184410953560a0658f1e02c6e
Andrea Belcore <an.belcore@studenti.unina.it>
2018-05-24 23:49:18 +0200

    CoreData is working

---
+++ Final Challenge/Controller/ExploreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreViewController.swift
+//  Final Challenge
+//
+//  Created by Andrea Belcore on 24/05/18.
+//  Copyright © 2018 Gian Marco Orlando. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
