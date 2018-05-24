854e7c517a14e682bcde03ecc633beb67595f6ce
Gian Marco Orlando <gian.orlando@studenti.unina.it>
2018-05-24 15:46:32 +0200

    Tra una roadmap ed uno step, speriamo che oggi Stefano si fa 'na chiavet

--- Final Challenge/View/CollectionCell/CustomLinksTableViewCell.swift
+++ Final Challenge/View/CollectionCell/CustomLinksTableViewCell.swift
@@ -11,10 +11,12 @@ import UIKit
 class CustomLinksTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var readingTimeLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -22,4 +24,25 @@ class CustomLinksTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+
+        //Modify the image of the checkButton basing on its state
+        if checkButton.isSelected == false  {
+            sender.setImage(UIImage(named: "Checkmark"), for: .selected)
+        }
+
+        //Animate the checkButton
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+
+        }) { (success) in
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                sender.isSelected = !sender.isSelected
+                sender.transform = .identity
+            }, completion: nil)
+        }
+    }
+
+
 }
