7e85abe6117e2c566484a0ecdcbb64ccfcec8e3f
Andrea Belcore <an.belcore@studenti.unina.it>
2018-05-24 14:49:38 +0200

    Solved AppDelegate dependence of CoreDataController

--- Final Challenge/Storage/CoreDataController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CoreDataController.swift
-//  Final Challenge
-//
-//  Created by Andrea Belcore on 24/05/18.
-//  Copyright © 2018 Gian Marco Orlando. All rights reserved.
-//
-
-import Foundation
