ec5a9b76ced353d860eac809d3fc268c12963a7b
Gian Marco Orlando <gian.orlando@studenti.unina.it>
2018-05-27 22:18:14 +0200

    Add the "Add resource" section :D

---
+++ Final Challenge/View/CollectionCell/AddResourceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddResourceTableViewCell.swift
+//  Final Challenge
+//
+//  Created by Gian Marco Orlando on 27/05/18.
+//  Copyright © 2018 Gian Marco Orlando. All rights reserved.
+//
+
+import UIKit
+
+class AddResourceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Final Challenge/View/CollectionCell/AddTagsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddTagsTableViewCell.swift
+//  Final Challenge
+//
+//  Created by Gian Marco Orlando on 27/05/18.
+//  Copyright © 2018 Gian Marco Orlando. All rights reserved.
+//
+
+import UIKit
+
+class AddTagsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
